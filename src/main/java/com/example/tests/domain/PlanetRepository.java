5886d9a0b3b2f84ba1e557a51c131f7664eb0ee0
jonas portugall <jonascportugal30@gmail.com>
2025-05-22 17:03:06 +0100

    adicioado o teste de unidade para cadastrar um plannet

--- src/main/java/com/example/tests/domain/PlanetRepository.java
+++ src/main/java/com/example/tests/domain/PlanetRepository.java
@@ -1,7 +1,9 @@
 package com.example.tests.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlanetRepository extends CrudRepository<Planet,Long> {
 
 }
---
+++ src/test/java/com/example/tests/domain/PlanetServiceTest.java
@@ -0,0 +1,30 @@
+package com.example.tests.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static com.example.tests.common.PlanetConstants.PLANET;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PlanetServiceTest {
+    @InjectMocks
+    private PlanetService planetService;
+
+    @Mock
+    private PlanetRepository planetRepository;
+
+    @Test
+    public void createPlanet_WithValidDate_returnPlanet(){
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+        Planet planet =  planetService.create(PLANET);
+        Assertions.assertThat(planet).isEqualTo(PLANET);
+    }
+}
